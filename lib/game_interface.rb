67130bda38d0df9c774db8f28a245e2faddb49ee
krypto <clayjeansen3@gmail.com>
2024-06-18 00:01:34 +0530

    feature: strike on each wrong guess added

--- lib/game_interface.rb
+++ lib/game_interface.rb
@@ -1,9 +1,11 @@
 require 'colorize'
 
 class Game
-attr_reader :hangman, :dash
-def initialize
+attr_reader :hangman, :dash, :word
+def initialize()
     @dash = []
+    @strike = 0
+    @word = nil
     @hangman = ["\u2502","\u2500"," ","\u250C","\u2510"," "," "," "," "]
     #gallows: 0, horizontal_line: 1, head: 2, corner: 3, rope: 4, left_arm: 5, right_arm: 6, body: 7, left_leg: 8, right_leg: 9
 end
@@ -18,7 +20,7 @@ puts "#{@hangman[0]}          #{@hangman[8]} #{@hangman[9]}"
 for i in (0..3)
     puts "#{@hangman[0]}"
 end
-
+puts "\n"
 end
 
 def random_word()
@@ -28,20 +30,78 @@ def random_word()
 end
 
 def blank_dash()
-    dash_length = random_word().length 
+    @word = "programming"
+    dash_length = @word.length 
 
     while dash_length > 0
-        @dash.push("__")
+        @dash.push("_")
         dash_length -= 1
         end
         @dash = @dash.join(' ')
 end
 
+def strike_on_wrong_guess
+    if @strike == 1
+        @hangman[2] = "\u25EF" #head
+    elsif @strike == 2
+        @hangman[7] = "\u2502" #body
+    elsif @strike == 3
+        @hangman[5] = "\u2571" #left_arm
+    elsif @strike == 4
+        @hangman[6] = "\u2572"  #right_arm
+    elsif @strike == 5
+        @hangman[8] = "\u2571"
+    elsif @strike == 6
+        @hangman[9] = "\u2572"
+    end
+
+end
+
+def startGame()
+    blank_dash()
+    word =  @word
+    puts @dash
+    until @strike > 7
+
+        if @strike > 6
+            puts "you have been hanged".colorize(:red)
+            break
+        elsif @dash.include?('_') == false
+                puts "Congrats! you Guessed the right word".colorize(:green)
+                break
+        end
+            
+        letter = gets.chomp
+        arr = word.split('').join(' ').split('')
+
+        if arr.include?(letter)
+            arr.each_with_index do |item,index|
+                if letter == item
+                    @dash[index] = letter
+                end
+            end
+        else
+            @strike +=  1
+            strike_on_wrong_guess()
+        end
+    
+        display_gameInterface()
+        puts @dash
+
+    end
+    puts "This correct word is #{@word}"
+end
+
 end
 
 hangman = Game.new
-p hangman.random_word
-puts hangman.blank_dash
+hangman.startGame
+#  hangman.blank_dash
+# puts hangman.word
+#  hangman.dash[0] = "R"
+#  hangman.dash[4] = "U"
+# puts hangman.dash
+
 
 # hangman.hangman[2] = "\u25EF"
 # hangman.hangman[7] = "\u2502"
